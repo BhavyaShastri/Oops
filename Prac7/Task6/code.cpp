3d66cd000874550314e07e9c3124eed394a6cfc2
BhavyaShastri <91321689+BhavyaShastri@users.noreply.github.com>
2021-10-19 16:03:08 +0530

    update

--- Prac7/Task6/code.cpp
+++ Prac7/Task6/code.cpp
@@ -1,17 +1,24 @@
 #include<iostream>
 using namespace std;
-int main()
-{
-   int i,j,n;
-   cout<<"enter number of rows";
-   cin>>n;
-   for(i=n;i>=1;i--)
-   {
-      for(j=1;j<=i;j++)
-       {
-         cout<<"*";
-       }
-       cout<<"\n";
-    }
+
+int main(){
+     int N;
+     cout << "Enter the number of rows : " << endl;
+     cin >> N;
+     char arr[N][N];
+     for(int i = 0 ; i < N ; i++){
+          for(int j = 0 ; j < N ; j++){
+               if(i <= j)
+                    arr[i][j] = '*';
+               else
+                    arr[i][j] = ' ';
+          }
+     }
+     //Printing the array
+     for(int i = 0 ; i < N ; i++){
+          for(int j = 0 ; j < N ; j++)
+               cout << arr[i][j] << " ";
+          cout << "\n";
+      }
      return 0;
-}
+}     
